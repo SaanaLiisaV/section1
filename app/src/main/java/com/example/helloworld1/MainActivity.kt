b059707c346f0fda271eff167591f333b9f2740c
SaanaLiisaV <saanaliisavain@gmail.com>
2026-04-16 12:01:57 +0300

    Layout

--- app/src/main/java/com/example/helloworld1/MainActivity.kt
+++ app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.helloworld1
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,14 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworld1.ui.theme.HelloWorld1Theme
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,15 +32,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorld1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(Modifier.padding(innerPadding)) {
-                        var teksti by remember { mutableStateOf("Hei maailma!")}
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        var teksti by remember { mutableStateOf("Hei!") }
 
-                    Button(onClick = { teksti = "Nappi painettu!" }) {
-                        Text("Paina minua!")
+                        Button(onClick = { teksti = "Hei maailma!" },
+
+                                modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth(0.6f))
+                        {
+
+                            Text(
+                                text = "Klikkaa tätä!",
+                                    modifier = Modifier.padding(8.dp))
+
+
+                        }
+                        Spacer(modifier = Modifier.fillMaxSize(0.2f))
+                        Text(
+                            text = teksti,
+                                    modifier = Modifier
+                                    .background(Color.Yellow)
+                                    .padding(16.dp)
+                        )
                     }
-                    Spacer(modifier = Modifier.fillMaxSize(0.2f))
-                    Text(teksti)
-                }
             }
         }
     }
